d90c907335802868437a4605a73abb72bce3137f
kolpakov <kolpakov@zelax.ru>
2020-05-10 17:19:56 +0300

    fix: Unused comment about insertion to the tree was deleted./*ФУНКЦИЯ ЗАПИСИ ЭЛЕМЕНТА В БИНАРНОЕ ДЕРЕВО*/

--- binary_search_tree/BinarySearchTree.cpp
+++ binary_search_tree/BinarySearchTree.cpp
@@ -27,18 +27,3 @@ void BinarySearchTree::addVertexToTree(int data, BinarySearchTree *&root_tree) {
   else
     addVertexToTree(data, root_tree->_left_edge);
 }
-
-// /*ФУНКЦИЯ ЗАПИСИ ЭЛЕМЕНТА В БИНАРНОЕ ДЕРЕВО*/
-// void push(int a, node **t)
-// {
-//     if ((*t) == NULL)                   //Если дерева не существует
-//     {
-//         (*t) = new node;                //Выделяем память
-//         (*t)->info = a;                 //Кладем в выделенное место аргумент a
-//         (*t)->l = (*t)->r = NULL;       //Очищаем память для следующего роста
-//         return;                         //Заложили семечко, выходим
-//     }
-//        //Дерево есть
-//         if (a > (*t)->info) push(a, &(*t)->r); //Если аргумент а больше чем текущий элемент, кладем его вправо
-//         else push(a, &(*t)->l);         //Иначе кладем его влево
-// }
